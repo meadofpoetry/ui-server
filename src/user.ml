1796b5b74aea5144a4d10e7dc397a690928f2a1c
Freyr <sky_rider_93@mail.ru>
2017-05-17 19:34:57 +0300

    simple cookie auth

--- src/user.ml
+++ src/user.ml
@@ -8,17 +8,20 @@ let pswd = "pswd"
    
 let auth_needed headers =
   let open Option in
-  Cohttp.Header.get_authorization headers
+  Cohttp.Cookie.Cookie_hdr.extract headers
+  |> List.find_pred (fun (name,_) -> name = "id")
   |> function
   | None  -> true
-  | Some (`Basic (name, pass)) -> false (*name = user && pass = pswd*)
-  | _ -> true
+  | Some _ -> false (*name = user && pass = pswd*)
 
 let auth headers name pass =
   (* if name = user && pass = pswd then*)
+  Lwt_io.print (Header.to_string headers) >>= fun _ ->
+  let nh = Header.add_list headers [(Cohttp.Cookie.Set_cookie_hdr.make ~domain:"127.0.0.1" ~path:"/" ("id", "name") |> Cohttp.Cookie.Set_cookie_hdr.serialize ~version:`HTTP_1_0)]
+  in Lwt_io.print (Header.to_string nh) >>= fun _ ->
   Server.respond_redirect
-         ~headers:(Cohttp.Header.add_authorization headers (`Basic (name, pass)))
-         ~uri:(Uri.with_path Uri.empty "/")
-         ()
+    ~headers:(nh)
+    ~uri:(Uri.with_path Uri.empty "/")
+    ()
  (* else Server.respond_redirect ~uri:(Uri.with_path Uri.empty "/login") ()
   *)
