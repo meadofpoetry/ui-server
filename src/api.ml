1a492f2fb16e054767d0782ae5f55fb97c9eb6fd
Freyr <sky_rider_93@mail.ru>
2017-05-19 11:58:48 +0300

    api: login get -> post

--- src/api.ml
+++ src/api.ml
@@ -8,6 +8,6 @@ let handle ~database
            meth path headers body =
   let redir = redirect_auth database headers in
   match meth, path with
-  | `GET,  ["auth"]  -> redir (fun _ -> home_page ()) (fun () -> Authorize.auth database headers)
-  | `POST, ["login"] -> Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"test" ()
+  | `POST, ["login"]  -> redir (fun _ -> home_page ()) (fun () -> Authorize.auth database headers)
+  | `GET,  ["test"]   -> Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"test" ()
   | _ -> not_found ()
