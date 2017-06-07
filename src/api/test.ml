ea38fda3e8bf71bdcc48e47ca9efc15e62064784
Freyr <sky_rider_93@mail.ru>
2017-06-07 11:12:52 +0300

    initial response

--- src/api/test.ml
+++ src/api/test.ml
@@ -6,6 +6,18 @@ let test _ _ _ _ body =
   let open Common in
   Cohttp_lwt_body.to_string body >>= fun body ->
   let jss = String.split_on_char '=' body |> fun l -> List.nth l 1 in
-  (*let js = Yojson.Safe.from_string jss in*)
-  Lwt_io.printf "Test body: %s\n" jss >>= fun _ ->
-  Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"Thank you, master!" ()
+  let js  = Uri.pct_decode jss |> Yojson.Safe.from_string in
+  let s =
+    Common.Qoe.Qoe_root.of_yojson js
+    |> function
+      | Error s -> "Sorry, something is wrong with your json"
+      | Ok (root : Qoe.Qoe_root.t) -> let prefix = "Thank you, master!\n" in
+                   begin match (CCOpt.get_exn root.graph).Qoe.Graph.state with
+                   | Some Null  -> prefix ^ "You want me to stop the graph?"
+                   | Some Stop  -> prefix ^ "Halting graph"
+                   | Some Play  -> prefix ^ "Starting"
+                   | Some Pause -> prefix ^ "Graph is going to be paused"
+                   | None       -> prefix ^ "I don't understand"
+                   end
+  in
+  Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:s ()
