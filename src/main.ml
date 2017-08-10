dc1d8e1d686b9425acb2f9783404269967efef90
Freyr <sky_rider_93@mail.ru>
2017-08-10 13:48:41 +0300

    initial pipeline_api implementation

--- src/main.ml
+++ src/main.ml
@@ -1,19 +1,20 @@
 
-let rec main config =
+let main config =
   Nocrypto_entropy_lwt.initialize () |> ignore;
-  
-  let db = Database.create config in
-  let pipe, pipeloop = Pipeline.create config db in
-  let routes = Api_handler.create (Pipeline_api.handlers pipe) in
-  let auth_filter = Redirect.redirect_auth db in
-  let server = Serv.create config auth_filter routes in
+  let rec mainloop () =
+    
+    let db = Database.create config in
+    let pipe, pipeloop = Pipeline.create config db in
+    let routes = Api_handler.create (Pipeline_api.handlers pipe) in
+    let auth_filter = Redirect.redirect_auth db in
+    let server = Serv.create config auth_filter routes in
 
-  let _ = Lwt_react.E.map (fun js -> Lwt_io.printf "Event: %s\n" (Yojson.Safe.to_string js)|> ignore) pipe.options_events in
-  Lwt_main.run (Lwt.join [server; pipeloop]);
+    Lwt_main.run (Lwt.join [server; pipeloop]);
 
-  Database.finalize db;
-  Pipeline.finalize pipe;
-  main config
+    Database.finalize db;
+    Pipeline.finalize pipe;
+    mainloop ()
+  in mainloop ()
 
 let () =
   let config = Config.create "./config.json" in
