574974f3d3264b516f1cd67d80e7bdaaa60443f4
Freyr <sky_rider_93@mail.ru>
2017-10-02 16:07:28 +0300

    ip_dektec/db added

--- boards/dvb_niit/lib/db.ml
+++ boards/dvb_niit/lib/db.ml
@@ -6,21 +6,21 @@ type _ req =
   | Store_measures : Board_types.measure -> unit Lwt.t req
 
 let init o =
-  Storage.Database.execute o [%sqlinit "CREATE TABLE IF NOT EXISTS dvb_meas( \
-                                        tun    INTEGER, \
-                                        lock   BOOL, \
-                                        power  REAL, \
-                                        mer    REAL, \
-                                        ber    REAL, \
-                                        freq   INTEGER, \
-                                        bitrate INTEGER, \
-                                        date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP \
-                                        );" ]
+  Storage.Database.execute o [%sqlinit {|CREATE TABLE IF NOT EXISTS dvb_meas(
+                                        tun     INTEGER,
+                                        lock    BOOL,
+                                        power   REAL,
+                                        mer     REAL,
+                                        ber     REAL,
+                                        freq    INTEGER,
+                                        bitrate INTEGER,
+                                        date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                        );|} ]
   >>= fun _ -> Lwt.return_unit
 
 let store_measures o (i, (m : rsp_measure)) =
-  Storage.Database.insert o [%sqlc "INSERT INTO dvb_meas(tun,lock,power,mer,ber,freq,bitrate)\
-                                    VALUES (%d,%b,%f?,%f?,%f?,%l?,%l?)"]
+  Storage.Database.insert o [%sqlc {|INSERT INTO dvb_meas(tun,lock,power,mer,ber,freq,bitrate)
+                                    VALUES (%d,%b,%f?,%f?,%f?,%l?,%l?)|}]
     i m.lock m.power m.mer m.ber m.freq m.bitrate
   >>= fun _ -> Lwt.return_unit
              
