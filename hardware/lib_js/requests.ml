4994d147ea419dd5eea2399676c5232c4f1475c8
alexander <sashayanin@gmail.com>
2017-11-16 17:54:27 +0300

    topology socket fix

--- hardware/lib_js/requests.ml
+++ hardware/lib_js/requests.ml
@@ -7,4 +7,4 @@ let get_topology () =
   >|= CCResult.(flat_map topology_of_yojson)
 
 let get_topology_socket () =
-  get_socket "api/hardware" topology_of_yojson
+  get_socket "api/hardware/topology_sock" topology_of_yojson
