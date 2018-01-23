e5edd762f2541f88c3df69b3da07e3e3e427a077
alexander <sashayanin@gmail.com>
2018-01-23 14:18:30 +0300

    added circular progress

--- common/components.ml
+++ common/components.ml
@@ -692,6 +692,38 @@ module Make
 
   end
 
+  module Circular_progress = struct
+
+
+    let sz                  = 50.
+    let base_class          = "mdc-circular-progress"
+    let circle_class        = CSS.add_element  base_class "circle"
+    let indeterminate_class = CSS.add_modifier base_class "indeterminate"
+    let primary_class       = CSS.add_modifier base_class "primary"
+    let secondary_class     = CSS.add_modifier base_class "secondary"
+
+    let create ?id ?style ?(classes=[]) ?attrs ?(indeterminate=true) ?(thickness=3.6) ?(size=40) () =
+      let style = Printf.sprintf "width: %dpx; height: %dpx" size size
+                  |> (fun x -> match style with
+                               | Some s -> x ^ "; " ^ s
+                               | None   -> x)
+      in
+      div ~a:([ a_class (classes
+                         |> cons_if indeterminate indeterminate_class
+                         |> CCList.cons base_class)
+              ; a_role ["progressbar"]]
+              |> add_common_attrs ?id ~style ?attrs)
+          [ svg ~a:([ Svg.a_class []
+                    ; Svg.a_viewBox (0.,0.,sz,sz)])
+                [ Svg.circle ~a:[ Svg.a_class [circle_class]
+                                ; Svg.a_cx (sz /. 2., None)
+                                ; Svg.a_cy (sz /. 2., None)
+                                ; Svg.a_fill `None
+                                ; Svg.a_stroke_width (thickness, None)
+                                ; Svg.a_r ((sz /. 2.) -. 5., None)] []]]
+
+  end
+
   module Linear_progress = struct
 
     let base_class           = "mdc-linear-progress"
