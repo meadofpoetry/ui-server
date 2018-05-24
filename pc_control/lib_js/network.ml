816dbdf8213898e282f60aabce86ed864b3cdb0b
Freyr <sky_rider_93@mail.ru>
2018-05-24 16:23:45 +0300

    Network_js: minor fixes

--- pc_control/lib_js/network.ml
+++ pc_control/lib_js/network.ml
@@ -25,14 +25,15 @@ let make_routes (routes : Network_config.address list) =
     item
   in
 
-  let list = new Item_list.t ~items:[] () in
+  let header = new Typography.Text.t ~font:Typography.Subheading_1 ~text:"Список статических маршрутов" () in
+  let list   = new Item_list.t ~items:[] () in
   
-  let address  = new Textfield.t ~input_id:"Адрес" ~input_type:Widget.IPV4 () in
-  let mask     = new Textfield.t ~input_id:"Маска подсети" ~input_type:(Widget.Integer (Some 0, Some 32)) () in
+  let address  = new Textfield.t ~input_id:"address-route" ~label:"Адрес" ~input_type:Widget.IPV4 () in
+  let mask     = new Textfield.t ~input_id:"mask-route" ~label:"Маска подсети" ~input_type:(Widget.Integer (Some 0, Some 32)) () in
   let add_but  = new Button.t ~label:"Добавить" () in
-  let add_box  = new Box.t ~widgets:[address#widget; mask#widget; add_but#widget] () in
+  let add_box  = new Box.t ~vertical:false ~widgets:[address#widget; mask#widget; add_but#widget] () in
 
-  let full_box = new Box.t ~widgets:[list#widget; add_box#widget] () in
+  let full_box = new Box.t ~widgets:[header#widget; list#widget; add_box#widget] () in
 
   let signal, push = React.S.create [] in
 
@@ -55,7 +56,11 @@ let make_routes (routes : Network_config.address list) =
     push [];
     List.iter add_route routes
   in
-  (*  set routes; init *)
+  let set_disabled flag =
+    address#set_disabled flag;
+    mask#set_disabled flag;
+    add_but#set_disabled flag
+  in
 
   Lwt_react.E.keep @@
     Lwt_react.E.map (fun _ ->
@@ -64,22 +69,25 @@ let make_routes (routes : Network_config.address list) =
         | _ -> ())
       add_but#e_click;
   
-  full_box, signal, set
+  full_box, signal, set, set_disabled
 
 let make_ipv4 (ipv4 : Network_config.ipv4_conf) =
   let ipv4_head  = new Card.Primary.t ~widgets:[new Card.Primary.title "Настройки IP" ()] () in
+
+  let meth = new Form_field.t
+               ~input:(new Switch.t ~input_id:"autoconf" ())
+               ~label:"Автоматическая настройка" ()
+  in
   
-  let meth       = new Switch.t ~input_id:"Автоматическая настройка" () in
-  
-  let address    = new Textfield.t ~input_id:"Адрес" ~input_type:Widget.IPV4 () in
-  let mask       = new Textfield.t ~input_id:"Маска подсети" ~input_type:(Widget.Integer (Some 0, Some 32)) () in
-  let gateway    = new Textfield.t ~input_id:"Шлюз" ~input_type:Widget.IPV4 () in
-  let routes, routes_s, routes_set = make_routes ipv4.routes.static in
+  let address    = new Textfield.t ~input_id:"address-ipv4" ~label:"Адрес" ~input_type:Widget.IPV4 () in
+  let mask       = new Textfield.t ~input_id:"mask-ipv4"  ~label:"Маска подсети" ~input_type:(Widget.Integer (Some 0, Some 32)) () in
+  let gateway    = new Textfield.t ~input_id:"gateway" ~label:"Шлюз" ~input_type:Widget.IPV4 () in
+  let routes, routes_s, routes_set, routes_disable = make_routes ipv4.routes.static in
 
   let signal, push = React.S.create ipv4 in
   
   let set (ipv4 : Network_config.ipv4_conf) =
-    meth#set_checked (ipv4.meth = Auto);
+    meth#input_widget#set_checked (ipv4.meth = Auto);
     address#fill_in @@ fst ipv4.address;
     mask#fill_in (Int32.to_int @@ snd ipv4.address);
     CCOpt.iter gateway#fill_in ipv4.routes.gateway;
@@ -91,24 +99,35 @@ let make_ipv4 (ipv4 : Network_config.ipv4_conf) =
   Lwt_react.S.keep @@
     Lwt_react.S.map (fun disabled -> address#set_disabled disabled;
                                      mask#set_disabled disabled;
-                                     gateway#set_disabled) meth#s_state;
-  
+                                     gateway#set_disabled disabled;
+                                     routes_disable disabled) meth#input_widget#s_state;
+
+  (* disable routes on gateway config *)
+  Lwt_react.S.keep @@
+    Lwt_react.S.map (function None -> routes_disable false | _ -> routes_disable true) gateway#s_input;
+
+  let media      = new Card.Media.t ~widgets:[new Box.t ~vertical:true ~widgets:[ meth#widget;
+                                                                                  address#widget;
+                                                                                  mask#widget;
+                                                                                  gateway#widget;
+                                                                                  routes#widget ] ()
+                     ] ()
+  in
+  media#style##.margin := Js.string "15px";
   let ipv4_sets  = new Card.t
                      ~widgets:[ ipv4_head#widget
-                              ; (new Card.Media.t ~widgets:[new Box.t ~vertical:true ~widgets:[ address#widget;
-                                                                                                mask#widget;
-                                                                                                gateway#widget;
-                                                                                                routes#widget ] ()
-                                   ] ())#widget
+                              ; media#widget
                      ] ()
   in
   
-  let signal = Lwt_react.S.l5 (fun (config : Network_config.ipv4_conf) address mask gateway routes ->
+  let signal = Lwt_react.S.l6 (fun (config : Network_config.ipv4_conf) meth address mask gateway routes ->
                    { config with
-                     address =
+                     meth    = if meth then Auto else Manual
+                   ; address =
                        (CCOpt.get_or address ~default:(fst config.address), CCOpt.get_or mask ~default:(snd config.address))
                    ; routes = { gateway = gateway; static = routes } }) (* TODO fix *)
                  signal
+                 meth#input_widget#s_state
                  address#s_input
                  (Lwt_react.S.map (CCOpt.map Int32.of_int) mask#s_input)
                  gateway#s_input
