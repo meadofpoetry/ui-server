fd2f379e21e561974d49294bb1e992a0234508a8
Freyr <sky_rider_93@mail.ru>
2018-06-07 18:53:39 +0300

    initial angstrom parser

--- usb_device/usb_device.ml
+++ usb_device/usb_device.ml
@@ -1,45 +1,84 @@
 open Containers
 open Lwt.Infix
+(*
+[@@@ocaml.warning "-32"]
 
-       [@@@ocaml.warning "-32"]
+[%%cstruct
+type header =
+  { prefix : uint16_t
+  ; port   : uint8_t
+  ; length : uint8_t
+  } [@@big_endian]]
 
-       [%%cstruct
-        type header =
-          { prefix : uint16_t
-          ; port   : uint8_t
-          ; length : uint8_t
-          } [@@big_endian]]
-
-       [@@@ocaml.warning "+32"]
+[@@@ocaml.warning "+32"]
+ *)
 
 type 'a cc = 'a Meta_board.cc
            
-type t = { dispatch : (int * (Cstruct.t list -> 'c cc as 'c) cc) list ref
+type t = { dispatch : (int * (Cstruct.t list -> 'c cc as 'c) cc) list ref (* TODO why list? *)
          ; send     : int -> Cstruct.t -> unit Lwt.t
          ; usb      : Cyusb.t
          }
 
-type header = { len    : int
-              ; port   : int
-              }
-
 let prefix = 0x44BB
 
-let to_header port parity length =
-  let hdr = Cstruct.create sizeof_header in
-  let ()  = set_header_prefix hdr prefix in
-  let ()  = set_header_port hdr ((if parity then 0x10 else 0) lor (port land 0xF)) in
-  let ()  = set_header_length hdr length in
-  hdr
+let msg_parser =
+  let open Angstrom in
+  let prefix_parser =
+    BE.uint16 >>= fun v ->
+    if v = prefix then return () else fail "Not a prefix"
+  in
+  let len_parser =
+    any_uint8 >>= fun port' ->
+    any_uint8 >>= fun length ->
+    let parity    = port' land 0x10 > 0 in
+    let port      = port' land 0xF in
+    let len       = (length * 2) - (if parity then 1 else 0) in
+    return (port,len)
+  in
+  let header_parser =
+    prefix_parser *> len_parser
+  in
+  let msg_parser' = 
+    header_parser >>= fun (port,len) ->
+    take_bigstring len >>= fun msg ->
+    return (port, Cstruct.of_bigarray msg)
+  in
+  many @@ fix (fun p ->
+              msg_parser' <|> (any_char *> p))
 
-let serialize port buf =
-  let buf_len = Cstruct.len buf in
+let parse_msg ?(prev : Angstrom.Buffered.unconsumed option) s =
+  let open Angstrom in
+  let state  = Buffered.parse msg_parser in
+  let state' = match prev with
+    | None -> state
+    | Some x -> Buffered.feed state (`Bigstring (Bigarray.Array1.sub x.buf x.off x.len))
+  in
+  Buffered.feed state' (`Bigstring (Cstruct.to_bigarray s))
+  |> (fun state -> Buffered.feed state (`Eof))
+  |> fun state ->
+    let res = Buffered.state_to_result state in
+    let unc = Option.(>>=) (Buffered.state_to_unconsumed state) (fun v -> if v.len = 0 then None else Some v) in
+    (unc, res)
+
+let msg_serializer t (port, msg) =
+  let open Faraday in
+  let buf_len = Cstruct.len msg in
   let parity  = (buf_len mod 2) <> 0 in
   let len     = (buf_len / 2) + (if parity then 1 else 0) in
-  let buf'    = if parity then Cstruct.append buf (Cstruct.create 1) else buf in
-  Cstruct.append (to_header port parity len) buf'
+  let buf     = if parity then Cstruct.append msg (Cstruct.create 1) else msg in
+  BE.write_uint16 t prefix;
+  write_uint8 t ((if parity then 0x10 else 0) lor (port land 0xF));
+  write_uint8 t len;
+  write_bigstring t ~off:(buf.off) ~len:(buf.len) (buf.buffer)
+  
+let serialize_msg (port, msg) =
+  let open Faraday in
+  let buf = Faraday.create 0x4096 in
+  msg_serializer buf (port, msg);
+  Cstruct.of_bigarray @@ serialize_to_bigstring buf
 
-
+  (*
 type err = Bad_prefix           of int
          | Bad_length           of int
          | Insufficient_payload of Cstruct.t
@@ -52,39 +91,8 @@ let string_of_err = function
   | Insufficient_payload _ -> "insufficient payload"
   | No_prefix_after_msg    -> "no prefix after message"
   | Unknown_err s          -> s
-
-let check_prefix buf =
-  let prefix' = get_header_prefix buf in
-  if prefix <> prefix' then Error (Bad_prefix prefix') else Ok buf
-
-let check_length buf =
-  let hdr,buf'  = Cstruct.split buf sizeof_header in
-  let length    = get_header_length hdr in
-  if (length <= 0) || (length > 256)
-  then Error (Bad_length length)
-  else let port'     = get_header_port hdr in
-       let parity    = port' land 0x10 > 0 in
-       let port      = port' land 0xF in
-       let len       = (length * 2) - (if parity then 1 else 0) in
-       try
-         let msg',rest = Cstruct.split buf' (length * 2) in
-         let msg,_     = Cstruct.split msg' len in
-         Ok ((port,msg),rest)
-       with _ -> Error (Insufficient_payload buf)
-
-let check_next_prefix ((_,rest) as x) =
-  if Cstruct.len rest < sizeof_header then Ok x
-  else (match check_prefix rest with
-        | Ok _    -> Ok x
-        | Error _ -> Error (No_prefix_after_msg))
-
-let get_msg buf =
-  try
-    Result.(check_prefix buf
-            >>= check_length
-            >>= check_next_prefix)
-  with e -> Error (Unknown_err (Printexc.to_string e))
-
+   *)
+(*
 let deserialize acc buf =
   let buf = (match acc with
              | Some x -> Cstruct.append x buf
@@ -100,13 +108,14 @@ let deserialize acc buf =
     else acc,b in
   let msgs,new_acc = f [] buf in
   (if Cstruct.len new_acc > 0 then Some new_acc else None), msgs
-
+ *)
+  
 let recv usb =
   Lwt_preemptive.detach (fun () -> Cyusb.recv usb)
 
 let send usb port =
   let send' port data =
-    let msg = serialize port data in
+    let msg = serialize_msg (port, data) in
     Cyusb.send usb msg
   in
   Lwt_preemptive.detach (send' port)
@@ -130,8 +139,11 @@ let create ?(sleep = 1.) () =
   let rec loop acc () =
     Lwt_unix.sleep sleep >>= fun () ->
     recv () >>= fun buf ->
-    let new_acc, msgs = deserialize acc buf in
-    dispatch := apply !dispatch msgs;
+    let new_acc, msgs = parse_msg ?prev:acc buf in
+    begin match msgs with
+    | Error e -> Logs.err (fun m -> m "(Usb_device) deserialize loop error: %s" e)
+    | Ok msgs -> dispatch := apply !dispatch msgs
+    end;
     loop new_acc ()
   in
   { usb; dispatch; send }, (fun () -> loop None ())
