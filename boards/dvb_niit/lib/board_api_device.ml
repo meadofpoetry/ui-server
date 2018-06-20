1f7a9321fa26a2db17031630db925214d653dbd7
alexander <sashayanin@gmail.com>
2018-06-20 16:54:56 +0300

    [WIP] working on device api

--- boards/dvb_niit/lib/board_api_device.ml
+++ boards/dvb_niit/lib/board_api_device.ml
@@ -23,4 +23,38 @@ end
 
 module HTTP = struct
 
+  let post_reset (api:api) () =
+    api.reset () >|= Result.return
+    >>= Json.respond_result_unit
+
+  let devinfo (api:api) () =
+    api.get_devinfo () >|= (devinfo_opt_to_yojson %> Result.return)
+    >>= Json.respond_result
+
+  let config (api:api) () =
+    api.get_config () >|= (config_to_yojson %> Result.return)
+    >>= Json.respond_result
+
+  let state_now (events:events) () =
+    React.S.value events.state
+    |> Common.Topology.state_to_yojson
+    |> Result.return
+    |> Json.respond_result
+
+  module Archive = struct
+
+    let state time (query:Uri.Query.t) () =
+      respond_error ~status:`Not_implemented "not_implemented" ()
+
+  end
+
 end
+
+let handler api events id meth ({path;query;_}:Uri.sep) sock_data headers body =
+  let is_guest = Common.User.eq id `Guest in
+  match Api.Headers.is_ws headers,meth,path with
+  (* WS *)
+  | true, `GET, ["state"] -> WS.state sock_data events body ()
+  (* HTTP *)
+  | false,`POST,["reset"] -> redirect_if is_guest @@ HTTP.post_reset api
+  | _ -> not_found ()
