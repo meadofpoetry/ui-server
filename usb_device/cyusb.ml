d03054e8459ae292559ad2f95dc54f8b6ca56fde
alexander <alexander@localhost.localdomain>
2018-07-05 12:44:19 +0300

    gitignore update + previous errorneous usb commit fix

--- usb_device/cyusb.ml
+++ usb_device/cyusb.ml
@@ -12,43 +12,42 @@ type t = { handle  : Cyusb_raw.handl
          }
               
 let create ?(inp = in_point) ?(outp = out_point) () =
-  (* let i = Cyusb_raw.init () in
-   * if i <> 1 (\* Only one device should be connected *\)
-   * then failwith (Printf.sprintf "Usb init-tion failure %d" i); *)
+  let i = Cyusb_raw.init () in
+  if i <> 1 (* Only one device should be connected *)
+  then failwith (Printf.sprintf "Usb init-tion failure %d" i);
   let handle = Cyusb_raw.get_handle 0 in
-  (* if Cyusb_raw.get_vendor handle <> (Unsigned.UShort.of_int 0x04b4)
-   * then failwith "Cypress chipset not detected";
-   * if Cyusb_raw.kernel_driver_active handle 0 <> 0
-   * then failwith "kernel driver active. Exitting";
-   * if Cyusb_raw.claim_interface handle 0 <> 0
-   * then failwith "Error in claiming interface";
-   * let in_max = Cyusb_raw.get_max_iso_packet_size handle (Unsigned.UChar.of_int inp) in
-   * let out_max = Cyusb_raw.get_max_iso_packet_size handle (Unsigned.UChar.of_int outp) in *)
+  if Cyusb_raw.get_vendor handle <> (Unsigned.UShort.of_int 0x04b4)
+  then failwith "Cypress chipset not detected";
+  if Cyusb_raw.kernel_driver_active handle 0 <> 0
+  then failwith "kernel driver active. Exitting";
+  if Cyusb_raw.claim_interface handle 0 <> 0
+  then failwith "Error in claiming interface";
+  let in_max = Cyusb_raw.get_max_iso_packet_size handle (Unsigned.UChar.of_int inp) in
+  let out_max = Cyusb_raw.get_max_iso_packet_size handle (Unsigned.UChar.of_int outp) in
   { handle
   ; inp
   ; outp
-  ; in_max = 0
-  ; out_max = 0
+  ; in_max
+  ; out_max
   }
 
 let finalize = Cyusb_raw.close
 
-let send usb b = ()
-  (* let open Cbuffer in
-   * let open Ctypes in
-   * let got     = allocate int32_t 0l in
-   * let buf_lst = Cbuffer.split_size usb.out_max b in
-   * 
-   * let rec send' = function
-   *   | [] -> ()
-   *   | x::xs ->
-   *      let _ = Cyusb_raw.bulk_transfer usb.handle (Unsigned.UChar.of_int usb.outp)
-   *                                      (get_ptr x) (len x)
-   *                                      got 2
-   *      in send' xs
-   * in
-   * send' buf_lst *)
-    
+let send usb b =
+  let open Cbuffer in
+  let open Ctypes in
+  let got     = allocate int32_t 0l in
+  let buf_lst = Cbuffer.split_size usb.out_max b in
+  
+  let rec send' = function
+    | [] -> ()
+    | x::xs ->
+       let _ = Cyusb_raw.bulk_transfer usb.handle (Unsigned.UChar.of_int usb.outp)
+                                       (get_ptr x) (len x)
+                                       got 2
+       in send' xs
+  in
+  send' buf_lst
 
 let recv usb = Cstruct.create 0
   (* let open Cbuffer in
