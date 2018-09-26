da4333b374a83900ca1715f0f74efa0403474348
Freyr666 <sky_rider_93@mail.ru>
2018-09-26 14:53:08 +0300

    minor ref

--- pipeline/lib/pipeline_protocol.ml
+++ pipeline/lib/pipeline_protocol.ml
@@ -6,7 +6,7 @@ open Msg_conv
 open Message
 open Notif
 open Qoe_errors
-
+(*
 let limit f e =
   let limiter = ref Lwt.return_unit in
   let delayed = ref None in
@@ -31,9 +31,7 @@ let limit f e =
       e
   in
   React.E.select [iter; event]
-   
-let (%) = Fun.(%)
-(* TODO make 'active type label *)
+ *)
         
 type options = { wm         : Wm.t Storage.Options.storage
                ; structures : Structure.Structures.t Storage.Options.storage
@@ -55,13 +53,17 @@ type channels =
   ; streams   : Structure.t list channel
   ; settings  : Settings.t channel
   }
+
+type notifs =
+  { streams   : Structure.t list signal
+  ; settings  : Settings.t signal
+  ; wm        : Wm.t signal
+  (*; graph     : Graph.t event*)
+  ; vdata     : Video_data.t event (* TODO to be split by purpose later *)
+  ; adata     : Audio_data.t event
+  }
   
-type api = { streams   : Structure.t list signal
-           ; settings  : Settings.t signal
-           ; graph     : Graph.t event
-           ; wm        : Wm.t signal
-           ; vdata     : Video_data.t event (* TODO to be split by purpose later *)
-           ; adata     : Audio_data.t event
+type api = { notifs    : notifs
            ; requests  : channels
            ; model     : Model.t
            }
@@ -73,7 +75,7 @@ module Settings_options = Storage.Options.Make(Settings)
 module Wm_notif = Notif.Make(Wm)
 module Structures_notif = Notif.Make(Structure.Structures)
 module Settings_notif = Notif.Make(Settings)
-module Graph_notif = Notif.Make(Graph)
+(*module Graph_notif = Notif.Make(Graph)*)
 module Video_data_notif = Notif.Make(Video_data)
 module Audio_data_notif = Notif.Make(Audio_data)
 
@@ -81,33 +83,49 @@ module Wm_msg = Message.Make(Wm)
 module Structure_msg = Message.Make(Structure.Structures)
 module Settings_msg = Message.Make(Settings)
 
+let create_send (type a) (typ : a typ) (conv : a converter) msg_sock : (a -> (a, exn) result Lwt.t) =
+  let mutex = Lwt_mutex.create () in
+  fun x ->
+  Lwt_mutex.with_lock mutex (fun () ->
+      Lwt.catch
+        (fun () -> Lwt_unix.with_timeout 0.5 (fun () ->
+                       Socket.send msg_sock (conv.to_string x) >>= fun () ->
+                       (* Logs.debug (fun m -> m "(Pipeline) <Json send> msg was sent"); *)
+                       Socket.recv msg_sock >|= fun resp ->
+                       (* Logs.debug (fun m -> m "(Pipeline) <Json send> resp was received"); *)
+                       Ok (conv.of_string resp)))
+        Lwt.return_error)
+                    
 let settings_init typ send (options : options) =
   let set_chan = Settings_msg.create typ send in
   Lwt.ignore_result @@ (set_chan.set options.settings#get
                         >>= function  Ok ()   -> Lwt_io.printf "Settings resp: fine\n"
                                     | Error r -> Lwt_io.printf "Settings resp: %s\n" r)
-  
-(* TODO test this *)
-let combine_and_set name combine opt set push data =
-  let nv = combine ~set:(opt#get) data in
-  match nv with
-  | `Kept v    -> Logs.debug (fun m -> m "(Pipeline) settings for %s were not changed" name); push v
-  | `Changed v -> 
-     Lwt_main.run (set v >>= function
-                   | Ok ()   ->
-                      Logs.debug (fun m -> m "(Pipeline) settings for %s were changed" name);
-                      Lwt.return @@ push v
-                   | Error e ->
-                      Logs_lwt.err (fun m -> m "(Pipeline) combine and set: failed to set data %s" e))
-
-let storage name combine opt set events =
-  let s, push = React.S.create opt#get in
-  let events  = limit (fun () -> Lwt_unix.sleep 0.5) events in
-  Lwt_react.E.keep @@
-    Lwt_react.E.map (combine_and_set name combine opt set push) events;
-  s
 
 let add_storages typ send options structs wm settings =
+  (* TODO test this *)
+  let combine_and_set name combine opt set push data =
+    let nv = combine ~set:(opt#get) data in
+    match nv with
+    | `Kept v    ->
+       Logs.debug (fun m -> m "(Pipeline) settings for %s were not changed" name);
+       push v
+    | `Changed v -> 
+       Lwt_main.run (set v >>= function
+                     | Ok ()   ->
+                        Logs.debug (fun m -> m "(Pipeline) settings for %s were changed" name);
+                        Lwt.return @@ push v
+                     | Error e ->
+                        Logs_lwt.err (fun m -> m "(Pipeline) combine and set: failed to set data %s" e))
+  in
+  let storage name combine opt set events =
+    let s, push = React.S.create opt#get in
+    let events  = Lwt_react.E.limit (fun () -> Lwt_unix.sleep 0.5) events in
+    Lwt_react.E.keep @@
+      Lwt_react.E.map (combine_and_set name combine opt set push) events;
+    s
+  in
+  
   let str_chan   = Structure_msg.create typ send in
   let wm_chan    = Wm_msg.create typ send in
   let set_chan   = Settings_msg.create typ send in
@@ -115,37 +133,6 @@ let add_storages typ send options structs wm settings =
   let wm         = storage "Wm" Wm.combine options.wm wm_chan.set wm in
   let settings   = storage "Settings" Settings.combine options.settings set_chan.set settings in
   structures, wm, settings
-  
-let notif_events typ =
-  let events, epush     = E.create () in
-  let ready, ready_push = E.create () in
-  let strm, strm_push   = E.create () in
-  let sets, sets_push   = E.create () in
-  let grap, grap_push   = E.create () in
-  let wm  , wm_push     = E.create () in
-  let vdata, vdata_push = E.create () in
-  let adata, adata_push = E.create () in  
-  let (<$>) f result =
-    match result with
-    | Ok r -> (f r : unit)
-    | Error e -> Logs.err (fun m -> m "(Pipeline) notification parse error %s" e)
-  in
-
-  let table = Hashtbl.create 10 in
-  List.iter (fun (n,f) -> Hashtbl.add table n f)
-    [ Notif.Ready.create typ ((<$>) ready_push)
-    ; Wm_notif.create typ ((<$>) wm_push)
-    ; Structures_notif.create typ ((<$>) strm_push)
-    ; Settings_notif.create typ ((<$>) sets_push)
-    ; Graph_notif.create typ ((<$>) grap_push)
-    ; Video_data_notif.create typ ((<$>) vdata_push)
-    ; Audio_data_notif.create typ ((<$>) adata_push)
-    ];
-
-  let dispatch = dispatch typ table in
-  Lwt_react.E.keep @@ E.map dispatch events;
-  epush, ready, strm, sets, grap, wm, vdata, adata,
-  strm_push, wm_push, sets_push
   
 let create_channels
       typ
@@ -179,18 +166,46 @@ let create_channels
   ; settings = { set_chan with set = set_set_upd }
   }
 
-let create_send (type a) (typ : a typ) (conv : a converter) msg_sock : (a -> (a, exn) result Lwt.t) =
-  let mutex = Lwt_mutex.create () in
-  fun x ->
-  Lwt_mutex.with_lock mutex (fun () ->
-      Lwt.catch
-        (fun () -> Lwt_unix.with_timeout 0.5 (fun () ->
-                       Socket.send msg_sock (conv.to_string x) >>= fun () ->
-                       (* Logs.debug (fun m -> m "(Pipeline) <Json send> msg was sent"); *)
-                       Socket.recv msg_sock >|= fun resp ->
-                       (* Logs.debug (fun m -> m "(Pipeline) <Json send> resp was received"); *)
-                       Ok (conv.of_string resp)))
-        Lwt.return_error)
+let init_exchange (type a) (typ : a typ) send structures_packer options =
+  let create_table lst =
+    let table = Hashtbl.create 10 in
+    List.iter (fun (n,f) -> Hashtbl.add table n f) lst;
+    table
+  in
+  let unwrap f result =
+    match result with
+    | Ok r -> (f r : unit)
+    | Error e -> Logs.err (fun m -> m "(Pipeline) notification parse error %s" e)
+  in
+  let events, epush     = E.create () in
+  let ready, ready_push = E.create () in
+  let strm, strm_push   = E.create () in
+  let sets, sets_push   = E.create () in
+  let wm  , wm_push     = E.create () in
+  let vdata, vdata_push = E.create () in
+  let adata, adata_push = E.create () in
+  let table =
+    create_table [ Notif.Ready.create typ (unwrap ready_push)
+                 ; Wm_notif.create typ (unwrap wm_push)
+                 ; Structures_notif.create typ (unwrap strm_push)
+                 ; Settings_notif.create typ (unwrap sets_push)
+                 ; Video_data_notif.create typ (unwrap vdata_push)
+                 ; Audio_data_notif.create typ (unwrap adata_push)
+      ]
+  in
+  let dispatch = dispatch typ table in
+  Lwt_react.E.keep @@ E.map dispatch events;
+
+  let requests =
+    create_channels typ send options structures_packer strm_push wm_push sets_push
+  in
+
+  let structures, wm, settings = add_storages typ send options strm wm sets in
+  let streams = S.map structures_packer structures in
+  let notifs = { streams; wm; settings; adata; vdata } in
+  
+  epush, ready, notifs, requests
+  
   
 let create (type a) (typ : a typ) db_conf config sock_in sock_out =
   let stor    = Storage.Options.Conf.get config in
@@ -214,6 +229,7 @@ let create (type a) (typ : a typ) db_conf config sock_in sock_out =
   let ready     = ref false in
   let proc      = None in
   let converter = (Msg_conv.get_converter typ) in
+  
   let send =
     let send = create_send typ converter msg_sock in
     fun msg -> if not !ready
@@ -221,30 +237,20 @@ let create (type a) (typ : a typ) db_conf config sock_in sock_out =
                else send msg
   in
 
-  let epush, ready_e,
-      structures, settings,
-      graph, wm,
-      vdata, adata,
-      strms_push, wm_push, sets_push = notif_events typ
-  in
-  let structures, wm, settings = add_storages typ send options structures wm settings in
- (* Lwt_react.E.keep @@
-    Lwt_react.E.map (fun _ -> Logs.debug (fun m -> m "(Pipeline) ready event")) ready_e; *)
-  let streams = S.map (Structure_conv.match_streams srcs) structures in
-  let requests =
-    let merge v = Structure_conv.match_streams srcs v in
-    create_channels typ send options merge strms_push wm_push sets_push
+  let merge v = Structure_conv.match_streams srcs v in
+  
+  let epush, ready_e, notifs, requests =
+    init_exchange typ send merge options
   in
 
-  let model = Model.create db_conf streams vdata adata in
+  let model = Model.create db_conf notifs.streams notifs.vdata notifs.adata in
   
-  let api = { streams
-            ; settings; graph; wm; vdata; adata
-            ; requests
-            ; model
-            } in
+  let api = { notifs; requests; model } in
   
-  let state = { ctx; msg; ev; options; srcs; proc; ready; ready_e } in
+  let state = { ctx; msg; ev;
+                options; srcs; proc;
+                ready; ready_e }
+  in
   let recv () =
     Socket.recv ev_sock
     >>= fun msg ->
