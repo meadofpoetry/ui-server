6e20b76829e6db0caecb4b055a4d7c91b3f25c03
Freyr666 <sky_rider_93@mail.ru>
2019-05-13 18:43:38 +0300

    initial encryption

--- application/user.ml
+++ application/user.ml
@@ -8,13 +8,28 @@ type users = { root     : userdata
              ; guest    : userdata
              } [@@deriving yojson, eq]
 
+(* TODO generate from build env *)
+let salt = Cstruct.of_hex "9b56e55328a4c97a250738f8dba1b992e8a1b508"
+
+let encrypt pass =
+  let password = Cstruct.of_string pass in
+  Pbkdf.pbkdf2 ~prf:(`SHA1) ~password ~salt ~count:4096 ~dk_len:361l
+  |> Cstruct.to_string
+
 module Config = struct
   type t = users
-  let default = { root     = { pass = "pswd" }
-                ; operator = { pass = "default" }
-                ; guest    = { pass = "default" }
+  let default = { root     = { pass = encrypt "pswd" }
+                ; operator = { pass = encrypt "pswd" }
+                ; guest    = { pass = encrypt "pswd" }
                 }
   let equal = equal_users
+
+  let map f { root; operator; guest } =
+    { root = { pass = f root }
+    ; operator = { pass = f operator }
+    ; guest = { pass = f guest }
+    }
+    
   (* TODO add encryption *)
   let to_string x =
     users_to_yojson x
@@ -42,9 +57,9 @@ let set_pass (storage : passwd) (pass_entry : pass) =
   let open Lwt.Infix in
   storage#get >>= fun table ->
   match pass_entry.user with
-  | `Root     -> storage#set { table with root     = { pass = pass_entry.password } }
-  | `Operator -> storage#set { table with operator = { pass = pass_entry.password } }
-  | `Guest    -> storage#set { table with guest    = { pass = pass_entry.password } }
+  | `Root     -> storage#set { table with root     = { pass = encrypt pass_entry.password } }
+  | `Operator -> storage#set { table with operator = { pass = encrypt pass_entry.password } }
+  | `Guest    -> storage#set { table with guest    = { pass = encrypt pass_entry.password } }
              
 let create (config : Kv.RW.t) =
   Passwd.create ~default:Config.default config ["users"]
@@ -56,6 +71,6 @@ let validate (storage : passwd) ~name ~pass =
     | Error e -> Lwt.return_error (`Unknown e)
     | Ok user ->
        get_pass storage user >>= fun actual_pass ->
-       if String.equal pass actual_pass.pass
+       if String.equal (encrypt pass) actual_pass.pass
        then Lwt.return_ok user
        else Lwt.return_error `Wrong_password
