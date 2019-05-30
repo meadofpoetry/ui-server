37fb16aab7cedddfcca04b096816681a342148db
Freyr666 <sky_rider_93@mail.ru>
2019-05-30 14:55:12 +0300

    new socket iface

--- lib/api/socket/api_websocket.mli
+++ lib/api/socket/api_websocket.mli
@@ -1,6 +1,38 @@
+module type CONTROL_MSG = sig
+  type t
+  val parse : t -> (int * string) option
+  val compose : int -> t -> t
+end
 
-module Make (User : Api.USER) (Body : Api.BODY) : sig
+module Json_msg : CONTROL_MSG with type t = Yojson.Safe.json
 
+module Make (User : Api.USER) (Body : Api.BODY) (Msg : CONTROL_MSG with type t = Body.t) : sig
+
+  module Api_http : module type of Api_cohttp.Make (User) (Body)
+
+  type t
+                                 
+  type event_node
+                                 
+  type node = Api_http.node
+
+  type user = Api_http.user                
+            
+  val event_node : ?doc:string
+                   -> ?restrict:user list
+                   -> path:('a, 'b)
+                        Netlib.Uri.Path.Format.t
+                   -> query:('b, user -> Body.t React.event Lwt.t)
+                        Netlib.Uri.Query.format
+                   -> 'a
+                   -> event_node
+
+  val make : ?prefix:string
+             -> event_node list
+             -> t
+
+  val to_http_node : ?doc:string -> prefix:string -> t -> node
+  (*
   module Api_http : module type of Api_cohttp.Make (User) (Body)
   
   type node = Api_http.node
@@ -34,5 +66,6 @@ module Make (User : Api.USER) (Body : Api.BODY) : sig
                   Netlib.Uri.Query.format
              -> 'a
              -> node
+   *)
 
 end
