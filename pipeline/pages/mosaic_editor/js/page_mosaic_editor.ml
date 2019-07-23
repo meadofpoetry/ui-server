19545171b667a47da55827eb8e13953f7cd2405e
monstasat <sashayanin@gmail.com>
2019-07-23 14:04:50 +0300

    removed test wm generator

--- pipeline/pages/mosaic_editor/js/page_mosaic_editor.ml
+++ pipeline/pages/mosaic_editor/js/page_mosaic_editor.ml
@@ -6,86 +6,12 @@ open Pipeline_http_js
 
 let ( >>= ) x f = Lwt_result.(map_err Api_js.Http.error_to_string @@ x >>= f)
 
-module Test = struct
-  let make_widget ?(type_ = Wm.Video)
-      ?(domain = Wm.Nihil)
-      ?aspect
-      ~x ~y ~w ~h () : string * Wm.widget =
-    let (position : Wm.position) = { x; y; w; h } in
-    string_of_int @@ Random.bits (),
-    { position = Some position
-    ; description = "Sample widget"
-    ; pid = Some 4096
-    ; type_
-    ; aspect
-    ; domain
-    ; layer = 0
-    }
-
-  let annotate_widgets ?(state = `Active) w =
-    List.map (fun (id, w) -> id, state, w) w
-
-  let make_container
-      ?(title = "Sample container")
-      ?(widgets = [])
-      ~position () : string * Wm.Annotated.state * Wm.Annotated.container =
-    title, `Active, { position; widgets }
-
-  let widgets =
-    [ make_widget ~type_:Audio ~x:0. ~y:0. ~w:50. ~h:50. ()
-    ; make_widget ~aspect:(16, 9) ~x:50. ~y:0. ~w:50. ~h:50. ()
-    ; make_widget
-        ~domain:(Chan { stream = Application_types.Stream.ID.make "id"
-                      ; channel = 2
-                      })
-        ~x:0. ~y:50. ~w:50. ~h:50. ()
-    ]
-
-  let containers =
-    [ make_container
-        ~title:"Россия 1"
-        ~position:{ x = 0.; y = 0.; w = 240.; h = 160. }
-        ~widgets:(annotate_widgets widgets)
-        ()
-    ; make_container
-        ~title:"ТНТ"
-        ~position:{ x = 240.; y = 0.; w = 520.; h = 160. }
-        ~widgets:(annotate_widgets widgets)
-        ()
-    ; make_container
-        ~title:"Канал"
-        ~position:{ x = 760.; y = 0.; w = 520.; h = 360. }
-        ~widgets:(annotate_widgets widgets)
-        ()
-    ; make_container
-        ~title:"Первый канал"
-        ~position:{ x = 0.; y = 160.; w = 760.; h = 560. }
-        ~widgets:(annotate_widgets widgets)
-        ()
-    ; make_container
-        ~title:"СТС"
-        ~position:{ x = 760.; y = 360.; w = 520.; h = 360. }
-        ~widgets:(annotate_widgets widgets)
-        ()
-    ]
-
-  let (wm : Wm.Annotated.t) =
-    { layout = containers
-    ; widgets = widgets
-    ; resolution = 1280, 720
-    }
-
-end
-
 let () =
   let open React in
   let scaffold = Scaffold.attach (Dom_html.getElementById "root") in
   let thread =
-    (* Lwt.return_ok Test.wm *)
     Http_wm.get_layout ()
-    >>= fun wm ->
-    let wm = { wm with widgets = Test.widgets @ wm.widgets } in
-    Http_structure.get_annotated ()
+    >>= fun wm -> Http_structure.get_annotated ()
     >>= fun streams ->
     Api_js.Websocket.JSON.open_socket ~path:(Uri.Path.Format.of_string "ws") ()
     >>= fun socket -> Http_wm.Event.get socket
