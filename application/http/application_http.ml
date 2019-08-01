7a7d4a0b747eba8847d90be3f0593ad00a65f9bc
monstasat <sashayanin@gmail.com>
2019-08-01 17:52:24 +0300

    added home page redirect

--- application/http/application_http.ml
+++ application/http/application_http.ml
@@ -115,80 +115,59 @@ let input topo (input : Topology.topo_input) =
      in
      (*`Index input.id,*)
      input_page, stream_page
-  
+
+let topo_page_path = "topology"
+
 let application_pages (app : Application.t) =
   let open Api_template in
-  let icon x =
-    let open Icon.SVG in
-    let path = create_path x () in
-    let icon = create [path] () in
-    Tyxml.Html.toelt icon
+  let hw_templates =
+    Boards.Board.Ports.fold (fun _ (x : Boards.Board.t) acc -> x.templates @ acc)
+      app.hw.boards []
   in
-  let props =
+  let topo = React.S.value app.topo in
+  let input_props, stream_templates =
+    List.split
+    @@ List.map (input topo)
+    @@ Topology.get_inputs topo
+  in
+  let topology_props =
     make_template_props
       ~title:"Конфигурация"
       ~pre_scripts:[Src "/js/ResizeObserver.js"]
       ~post_scripts:[Src "/js/page-topology.js"]
       ~stylesheets:["/css/page-topology.min.css"]
-      ()
-  in
-  let _demo_props =
-    make_template_props
-      ~title:"UI Демо"
-      ~pre_scripts:[ Src "/js/moment.min.js"
-                   ; Src "/js/Chart.min.js"
-                   ]
-      ~post_scripts:[Src "/js/demo.js"]
-      ~stylesheets:["/css/demo.min.css"]
-      ()
-  in
-  let topo = React.S.value app.topo in
-  let hw_templates =
-    Boards.Board.Ports.fold (fun _ (x : Boards.Board.t) acc -> x.templates @ acc)
-      app.hw.boards []
-  in
-  let inputs = Topology.get_inputs topo in
-  let input_templates, stream_templates =
-    List.map (input topo) inputs
-    |> List.split
-  in
+      () in
   subtree
     ~priority:(`Index 1)
     ~title:"Входы"
     ~icon:(icon Components_tyxml.Svg_icons.arrow_right_box)
-    (List.flatten input_templates)
+    (List.flatten input_props)
   @ simple
     ~priority:(`Index 4)
     ~title:"Конфигурация"
     ~icon:(icon Components_tyxml.Svg_icons.tournament)
-    ~path:(Path.of_string "application")
-    props
-  @ simple
-    ~priority:(`Index 5)
-    ~title:"Демо"
-    ~icon:(icon Components_tyxml.Svg_icons.material_design)
-    ~path:(Path.of_string "demo")
-    _demo_props
+    ~path:(Path.of_string topo_page_path)
+    topology_props
   @ List.flatten stream_templates
   @ hw_templates
 
 let application_handlers (app : Application.t) =
   let open Api_http in
-  make ~prefix:"topology" (* TODO change to application *)
+  make ~prefix:"application"
     [ node ~doc:"Sets streams that are received by PC process"
         ~restrict:[`Guest]
         ~meth:`POST
-        ~path:Path.Format.("stream_table" @/ empty)
+        ~path:Path.Format.("stream-table" @/ empty)
         ~query:Query.empty
         (Application_api.set_streams app)
     ; node ~doc:"Returns device topology"
         ~meth:`GET
-        ~path:Path.Format.empty
+        ~path:Path.Format.("topology" @/ empty)
         ~query:Query.empty
         (Application_api.get_topology app)
     ; node ~doc:"Returns stream table"
         ~meth:`GET
-        ~path:Path.Format.("stream_table" @/ empty)
+        ~path:Path.Format.("stream-table" @/ empty)
         ~query:Query.empty
         (Application_api.get_streams app)
     ; node ~doc:"Returns all streams"
@@ -198,7 +177,7 @@ let application_handlers (app : Application.t) =
         (Application_api.get_all_streams app)
     ; node ~doc:"Returns the source of a last suitable stream"
         ~meth:`GET
-        ~path:Path.Format.("source" @/ empty)
+        ~path:Path.Format.("stream-source" @/ empty)
         ~query:Query.["id", (module Single(Stream.ID))]
         (Application_api.get_stream_source app)
     ; node ~doc:"Log for input (and stream)"
@@ -219,13 +198,13 @@ let application_ws (app : Application.t) =
   let open Api_websocket in
   (* TODO add closing event *)
   (*let socket_table = Api_websocket.make_socket_table () in*)
-  make ~prefix:"topology" (* TODO change to application *)
+  make ~prefix:"application"
     [ event_node ~doc:"Pushes device topology to the client"
-        ~path:Path.Format.empty
+        ~path:Path.Format.("topology" @/ empty)
         ~query:Query.empty
         (Application_api.Event.get_topology app)
     ; event_node ~doc:"Pushes stream table to the client"
-        ~path:Path.Format.("stream_table" @/ empty)
+        ~path:Path.Format.("stream-table" @/ empty)
         ~query:Query.empty
         (Application_api.Event.get_streams app)
     ; event_node ~doc:"Log for input (and stream)"
@@ -233,6 +212,22 @@ let application_ws (app : Application.t) =
         ~query:Query.["input", (module List(Topology.Show_topo_input));
            "id", (module List(Stream.ID))]
         (Application_api.Event.get_log app)
+    ]
+
+let redirect_handlers () =
+  let open Api_http in
+  make
+    [ node ~doc:"Home page redirect"
+        ~meth:`GET
+        ~path:Path.Format.empty
+        ~query:Query.empty
+        (fun _user _body _env _state ->
+           let ( >>= ) = Lwt.bind in
+           let uri = Uri.make ~path:topo_page_path () in
+           let rsp =
+             Cohttp_lwt_unix.Server.respond_redirect ~uri ()
+             >>= fun x -> Lwt.return @@ `Response x
+           in Lwt.return @@ `Instant rsp)
     ]
 
 let tick ?(timeout = 1.) () =
@@ -250,10 +245,10 @@ let create templates (app : Application.t)
     foreing_handlers
     foreign_ws =
   let (>>=) = Lwt_result.bind in
-  
+
   Kv.RO.read templates [ "base.html" ]
   >>= fun template ->
-  
+
   let proc_pages = match app.proc with
     | None -> []
     | Some proc -> proc#pages () in
@@ -292,13 +287,14 @@ let create templates (app : Application.t)
     |> Api_http.make
   in
   let api = Api_http.merge ~prefix:"api"
-              ( foreing_handlers
-                :: user_handlers app.users
-                :: Pc_control_http.network_handlers app.network
-                :: application_api
-                :: board_api
-                :: proc_api_list)
+      ( foreing_handlers
+        :: user_handlers app.users
+        :: Pc_control_http.network_handlers app.network
+        :: application_api
+        :: board_api
+        :: proc_api_list)
   in
+  let redirect = redirect_handlers () in
   let ping, loop = tick () in
   let ws =
     Api_websocket.to_http ~prefix:"ws" ~ping
@@ -309,140 +305,4 @@ let create templates (app : Application.t)
         :: board_ws
         :: proc_ws_list )
   in
-  Lwt.return_ok (Api_http.merge [api; ws; pages], loop)
-
-    
- (*   
-let make_icon path =
-  let open Icon.SVG in
-  let path = create_path path () in
-  let icon = create [path] () in
-  Tyxml.Html.toelt icon
-
-let get_input_href (x : Topology.topo_input) =
-  let name = Topology.input_to_string x.input in
-  let id = string_of_int x.id in
-  Filename.concat name id
-
-let input topo (input : Topology.topo_input) =
-  let path =
-    List.find_map (fun (i, p, c) ->
-        if Topology.equal_topo_input i input
-        then Some (p, c) else None)
-      (Topology.get_paths topo) in
-  match path with
-  | None              -> failwith "input not found"
-  | Some (boards, cpu) ->
-     let title = Topology.get_input_name input in
-     let boards =
-       List.map (fun (x : Topology.topo_board) -> x.control, x.typ) boards
-       |> Topology.boards_to_yojson
-       |> Yojson.Safe.to_string in
-     let cpu =
-       Option.map (fun (x : Topology.topo_cpu) -> x.process) cpu
-       |> Json.Option.to_yojson Topology.process_type_to_yojson
-       |> Yojson.Safe.to_string in
-     let input_string = Topology.Show_topo_input.to_string input in
-     let input_template =
-       { title = Some title
-       ; pre_scripts  =
-           [ Raw (Printf.sprintf "var input = \"%s\";\
-                                  var boards = %s;\
-                                  var cpu = %s;"
-                    input_string boards cpu)]
-       ; post_scripts = [Src "/js/input.js"]
-       ; stylesheets = []
-       ; content = []
-       } in
-     let input_page =
-       `Index input.id,
-       Simple { title
-              ; icon = None
-              ; href = Uri.Path.of_string @@ get_input_href input
-              ; template = input_template } in
-     let pre = "input/" ^ get_input_href input in
-     let stream_template =
-       { title = Some ("Входы / " ^ title)
-       ; pre_scripts =
-           [ Raw (Printf.sprintf "var input = \"%s\";\
-                                  var boards = %s;\
-                                  var cpu = %s;"
-                    input_string boards cpu)
-           ; Src "/js/moment.min.js"
-           ; Src "/js/Chart.min.js"
-           ; Src "/js/chartjs-plugin-streaming.min.js"
-           ; Src "/js/chartjs-plugin-datalabels.min.js"
-           ]
-       ; post_scripts = [Src "/js/stream.js"]
-       ; stylesheets = []
-       ; content = []
-       } in
-     let stream_page =
-       `Index input.id,
-       Pure { path = Uri.Path.Format.(pre @/ Stream.ID.fmt ^/ empty)
-            ; template = stream_template } in
-     input_page, stream_page
-
-let create (app : Application.t)
-    : upper ordered_item list User.user_table =
-  let topo  = React.S.value app.topo in
-  let hw_templates =
-    Hardware.Map.fold (fun _ (x : Boards.Board.t) acc ->
-        List.cons_maybe x.templates acc) app.hw.boards [] in
-  let props =
-    { title = Some "Конфигурация"
-    ; pre_scripts = []
-    ; post_scripts = [Src "js/topology.js"]
-    ; stylesheets = ["/css/topology.min.css"]
-    ; content = []
-    } in
-  let _demo_props = (* TODO *)
-    { title = Some "UI Демо"
-    ; pre_scripts =
-        [ Src "/js/moment.min.js"
-        ; Src "/js/Chart.min.js"
-        ]
-    ; post_scripts = [Src "/js/demo.js"]
-    ; stylesheets = ["/css/demo.min.css"]
-    ; content = []
-    } in
-  let inputs = Topology.get_inputs topo in
-  let input_templates, stream_templates =
-    List.map (input topo) inputs
-    |> List.split in
-  let app_template =
-    [ `Index 2,
-      Subtree { title = "Входы"
-              ; icon = Some (make_icon Icon.SVG.Path.arrow_right_box)
-              ; href = Uri.Path.of_string "input"
-              ; templates = input_templates }
-    ; `Index 3,
-      Simple { title = "Конфигурация"
-             ; icon = Some (make_icon Icon.SVG.Path.tournament)
-             ; href = Uri.Path.of_string "application"
-             ; template = props }
-    (* ; `Index 4,
-     *   Simple  { title = "UI Демо"
-     *           ; icon = Some (make_icon Icon.SVG.Path.material_design)
-     *           ; href = Uri.Path.of_string "demo"
-     *           ; template = demo_props } *)
-    ]
-  in
-  let proc = match app.proc with
-    | None -> Common.User.empty_table
-    | Some p -> p#template ()
-  in
-  Common.User.concat_table
-    ([ Responses.home_template ()
-     ; User_template.create ()
-     ; Pc_control.Network_template.create ()
-     ; proc
-     ; { root = app_template
-       ; operator = app_template
-       ; guest = app_template }
-     ; { root = stream_templates
-       ; operator = stream_templates
-       ; guest = stream_templates }
-     ]
-     @ hw_templates)
-  *)
+  Lwt.return_ok (Api_http.merge [api; ws; pages; redirect], loop)
