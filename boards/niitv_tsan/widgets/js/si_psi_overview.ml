ce12aa26322400dcf8bb09a28528171703e9e0e0
monstasat <sashayanin@gmail.com>
2019-09-18 09:11:57 +0300

    minor si psi overview improvements

--- boards/niitv_tsan/widgets/js/si_psi_overview.ml
+++ boards/niitv_tsan/widgets/js/si_psi_overview.ml
@@ -1,4 +1,155 @@
 open Js_of_ocaml
 open Js_of_ocaml_tyxml
+open Components
+open Board_niitv_tsan_http_js
 include Board_niitv_tsan_widgets_tyxml.Si_psi_overview
 module Markup_js = Make (Tyxml_js.Xml) (Tyxml_js.Svg) (Tyxml_js.Html)
+module Markup_r = Make (Tyxml_js.R.Xml) (Tyxml_js.R.Svg) (Tyxml_js.R.Html)
+
+let ( >>= ) = Lwt.bind
+
+module Attr = struct
+  let data_control = "data-control"
+
+  let data_mode = "data-mode"
+end
+
+module Selector = struct
+  let menu_icon = "." ^ CSS.menu_icon
+
+  let placeholder = Printf.sprintf ".%s" Components_lab.Placeholder.CSS.root
+
+  let table = Printf.sprintf ".%s" CSS.table
+
+  let menu = "." ^ Menu.CSS.root
+end
+
+class t elt () =
+  object (self)
+    val control : int =
+      match Element.get_attribute elt Attr.data_control with
+      | None ->
+          failwith
+            (Printf.sprintf
+               "%s: no `%s` attribute found on root element"
+               CSS.root
+               Attr.data_control)
+      | Some x -> int_of_string x
+
+    val menu : Menu.t option =
+      Option.map Menu.attach @@ Element.query_selector elt Selector.menu
+
+    val menu_icon : Icon_button.t option =
+      Option.map Icon_button.attach @@ Element.query_selector elt Selector.menu_icon
+
+    val placeholder =
+      match Element.query_selector elt Selector.placeholder with
+      | Some x -> x
+      | None -> Tyxml_js.To_dom.of_div @@ Markup_js.create_empty_placeholder ()
+
+    val table : _ Gadt_data_table.t =
+      Gadt_data_table.attach ~fmt:(Markup_js.create_table_format ~hex:false ())
+      @@ Element.query_selector_exn elt Selector.table
+
+    val mutable hex = false
+
+    val mutable listeners = []
+
+    inherit Widget.t elt () as super
+
+    method! init () : unit =
+      self#update_empty_state ();
+      Option.iter (fun menu -> menu#set_quick_open true) menu;
+      super#init ()
+
+    method! initial_sync_with_dom () : unit =
+      listeners <-
+        (match menu, menu_icon with
+        | Some menu, Some menu_icon ->
+            [ Js_of_ocaml_lwt.Lwt_js_events.clicks menu_icon#root (fun _ _ ->
+                  menu#reveal ())
+            ; Menu.Lwt_js_events.selects menu#root self#handle_menu_selection_change ]
+        | _ -> [])
+        @ listeners;
+      super#initial_sync_with_dom ()
+
+    method! destroy () : unit =
+      table#destroy ();
+      Option.iter Widget.destroy menu;
+      Option.iter Widget.destroy menu_icon;
+      List.iter Lwt.cancel listeners;
+      super#destroy ()
+
+    method private update_empty_state () =
+      if table#rows_collection##.length = 0
+      then Dom.appendChild super#root placeholder
+      else Element.remove placeholder
+
+    method private set_hex (hex : bool) : unit =
+      let pid_fmt = Markup_js.pid_fmt ~hex in
+      let (format : _ Markup_js.Fmt.data_format) =
+        match table#data_format with
+        | _ :: tl -> pid_fmt :: tl
+      in
+      table#set_data_format ~redraw:false format;
+      List.iter
+        (fun row ->
+          let cells = row##.cells in
+          Js.Opt.iter
+            (cells##item 0)
+            (fun cell ->
+              Gadt_data_table.(set_cell_value pid_fmt (get_cell_value pid_fmt cell) cell)))
+        table#rows
+
+    method private reset_bitrate_stats () : unit Lwt.t =
+      let (scaffold : Scaffold.t) = Js.Unsafe.global##.scaffold in
+      let rec aux () =
+        Http_monitoring.reset_bitrate_stats control
+        >>= function
+        | Ok () ->
+            let snackbar =
+              Snackbar.make
+                ~label:(`Text "Статистика битрейта сброшена")
+                ~dismiss:`True
+                ()
+            in
+            scaffold#show_snackbar ~on_close:(fun _ -> snackbar#destroy ()) snackbar
+        | Error (`Msg msg) ->
+            let snackbar =
+              Snackbar.make
+                ~label:
+                  (`Text
+                    (Printf.sprintf
+                       "Не удалось сбросить статистику \
+                        битрейта. %s"
+                       msg))
+                ~action:(`Text "Повторить")
+                ()
+            in
+            scaffold#show_snackbar
+              ~on_close:(fun reason ->
+                (match reason with
+                | Action -> Lwt.async aux
+                | _ -> ());
+                snackbar#destroy ())
+              snackbar
+      in
+      aux ()
+
+    method private handle_menu_selection_change e _ : unit Lwt.t =
+      let detail = Widget.event_detail e in
+      if Element.has_class detail##.item CSS.bitrate_reset
+      then self#reset_bitrate_stats ()
+      else (
+        (match Element.get_attribute detail##.item Attr.data_mode with
+        | Some "hex" ->
+            hex <- true;
+            self#set_hex hex
+        | Some "dec" ->
+            hex <- false;
+            self#set_hex hex
+        | _ -> ());
+        Lwt.return_unit)
+  end
+
+let attach elt : t = new t (elt :> Dom_html.element Js.t) ()
--- boards/niitv_tsan/widgets/tyxml/si_psi_overview.ml
+++ boards/niitv_tsan/widgets/tyxml/si_psi_overview.ml
@@ -4,6 +4,12 @@ open Board_niitv_tsan_types
 
 module CSS = struct
   let root = Util.CSS.root ^ "-si-psi-overview"
+
+  let bitrate_reset = BEM.add_element root "bitrate-reset"
+
+  let table = BEM.add_element root "table"
+
+  let menu_icon = BEM.add_element root "menu-icon"
 end
 
 module Make
@@ -15,6 +21,9 @@ struct
   module Data_table_markup = Data_table.Make (Xml) (Svg) (Html)
   module Divider_markup = Divider.Make (Xml) (Svg) (Html)
   module Fmt = Data_table.Make_fmt (Xml)
+  module Icon_button_markup = Icon_button.Make (Xml) (Svg) (Html)
+  module Icon_markup = Icon.Make (Xml) (Svg) (Html)
+  module Placeholder_markup = Components_lab_tyxml.Placeholder.Make (Xml) (Svg) (Html)
 
   let dec_pid_fmt = Fmt.Int
 
@@ -35,7 +44,7 @@ struct
       ; is_numeric = true }
 
   (** Returns HTML element to insert into 'Extra' table column *)
-  let to_table_extra ?(hex = false) (id : SI_PSI_table.id) =
+  let create_table_id_ext ?(hex = false) (id : SI_PSI_table.id) =
     let to_id_string =
       match hex with
       | true -> Printf.sprintf "0x%02X"
@@ -52,32 +61,22 @@ struct
       | _ -> []
     in
     let data = Yojson.Safe.to_string (SI_PSI_table.id_to_yojson id) in
-    let wrap x =
-      let length = List.length x in
-      span
-        ~a:[a_user_data "id" data]
-        (List.mapi
-           (fun i (s, v) ->
-             let v = to_id_string v in
-             let v = if i = pred length then v else v ^ ", " in
-             span [span ~a:[a_class [Typography.CSS.subtitle2]] [txt (s ^ ": ")]; txt v])
-           x)
-    in
-    toelt (wrap specific)
+    let length = List.length specific in
+    span
+      ~a:[a_user_data "id" data]
+      (List.mapi
+         (fun i (s, v) ->
+           let v = to_id_string v in
+           let v = if i = pred length then v else v ^ ", " in
+           span [span ~a:[a_class [Typography.CSS.subtitle2]] [txt (s ^ ": ")]; txt v])
+         specific)
 
   let id_ext_fmt ~hex =
     Fmt.Custom_elt
       { is_numeric = false
       ; compare = SI_PSI_table.compare_id
-      ; to_elt = to_table_extra ~hex
+      ; to_elt = toelt % create_table_id_ext ~hex
       ; of_elt = (fun _ -> assert false) }
-
-  let section_fmt =
-    Fmt.Custom
-      { is_numeric = true
-      ; compare = (fun x y -> List.(Int.compare (length x) (length y)))
-      ; to_string = Fun.(string_of_int % List.length)
-      ; of_string = (fun _ -> assert false) }
 
   let create_table_format ?(hex = false) () : _ Data_table_markup.Fmt.format =
     let br_fmt = Fmt.Option (Float, "-") in
@@ -89,12 +88,20 @@ struct
       ; make_column ~title:"Доп. инфо" id_ext_fmt
       ; make_column ~sortable:true ~title:"Версия" Int
       ; make_column ~sortable:true ~title:"Сервис" (Option (String, ""))
-      ; make_column ~title:"Кол-во секций" section_fmt
+      ; make_column ~title:"Кол-во секций" Int
       ; make_column ~title:"LSN" Int
       ; make_column ~title:"Битрейт, Мбит/с" br_fmt
       ; make_column ~title:"%" pct_fmt
       ; make_column ~title:"Min, Мбит/с" br_fmt
       ; make_column ~title:"Max, Мбит/с" br_fmt ]
+
+  let create_empty_placeholder ?classes ?attrs () =
+    Placeholder_markup.create
+      ?classes
+      ?attrs
+      ~icon:(Icon_markup.SVG.create ~d:Svg_icons.emoticon_sad ())
+      ~text:(`Text "Не найдено ни одного PID")
+      ()
 
   let create ?(classes = []) ?(attrs = []) () =
     let classes = CSS.root :: classes in
