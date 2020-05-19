e0de3d5b9dc41da22bef18f248d394abfd2db8d1
Freyr666 <sky_rider_93@mail.ru>
2020-05-19 16:21:59 +0300

    timedate pages

---
+++ pc_control/pages/timedate_settings/tyxml/page_timedate_settings_tyxml.ml
@@ -0,0 +1,19 @@
+open Components_tyxml
+
+module CSS = struct
+  include Ui_templates_tyxml.Settings_page.CSS
+
+  let root = "timedate-settings"
+
+  let action = BEM.add_element root "action"
+end
+
+module Make
+    (Xml : Xml_sigs.NoWrap)
+    (Svg : Svg_sigs.NoWrap with module Xml := Xml)
+    (Html : Html_sigs.NoWrap with module Xml := Xml and module Svg := Svg) =
+struct
+  include Ui_templates_tyxml.Settings_page.Make (Xml) (Svg) (Html)
+end
+
+module F = Make (Tyxml.Xml) (Tyxml.Svg) (Tyxml.Html)
