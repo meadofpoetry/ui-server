0698ed7ed2b0c4f507cecc2e85568e264d808b9f
monstasat <sashayanin@gmail.com>
2020-06-07 23:32:37 +0300

    refactor: remove redundant label from timezone select

--- pc_control/pages/timedate_settings/tyxml/timezone.ml
+++ pc_control/pages/timedate_settings/tyxml/timezone.ml
@@ -13,9 +13,9 @@ module CSS = struct
 end
 
 module Make
-         (Xml : Xml_sigs.NoWrap)
-         (Svg : Svg_sigs.NoWrap with module Xml := Xml)
-         (Html : Html_sigs.NoWrap with module Xml := Xml and module Svg := Svg) =
+    (Xml : Xml_sigs.NoWrap)
+    (Svg : Svg_sigs.NoWrap with module Xml := Xml)
+    (Html : Html_sigs.NoWrap with module Xml := Xml and module Svg := Svg) =
 struct
   open Html
   open Xml.W
@@ -25,30 +25,26 @@ struct
   open Form_field.Make (Xml) (Svg) (Html)
 
   open Ui_templates_tyxml.Settings_page.Make (Xml) (Svg) (Html)
-  
+
   let create ?classes ?(a = []) (options : string list)
-        (v : Pc_control_types.Timedate_config.t) =
+      (v : Pc_control_types.Timedate_config.t) =
     let tz_selector =
       let tz_input_id = tz_id ^ "-id" in
       let classes = Option.map (fmap (fun x -> CSS.select :: x)) classes in
-      let options = List.map (fun opt ->
-                        if opt = v.timezone
-                        then Native.option ?classes ~text:opt ~selected:true ()
-                        else Native.option ?classes ~text:opt ())
-                      options
+      let options =
+        List.map
+          (fun opt ->
+            if opt = v.timezone then
+              Native.option ?classes ~text:opt ~selected:true ()
+            else Native.option ?classes ~text:opt ())
+          options
       in
       Native.select ~a:[ a_id tz_id ] ?classes ~outlined:true
-        ~input_id:tz_input_id
-        ~label:(return "Часовой пояс") ~options ()
+        ~input_id:tz_input_id ~options ()
     in
     let header =
       create_section_header ~title:(`Text "Часовой пояс") ()
     in
-    create_section
-      ?classes
-      ~a:(a_id id::a)
-      ~header
-      ~children:[ tz_selector ]
+    create_section ?classes ~a:(a_id id :: a) ~header ~children:[ tz_selector ]
       ()
-    
 end
