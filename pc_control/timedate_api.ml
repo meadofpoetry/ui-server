eee3bb95b335d6c4e7c98256ab5f6b85d5ed416c
Freyr666 <sky_rider_93@mail.ru>
2020-06-08 15:09:11 +0300

    Timedate: add lock and api call logging

--- pc_control/timedate_api.ml
+++ pc_control/timedate_api.ml
@@ -2,6 +2,8 @@ open Pc_control_types
 
 let ( let* ) = Lwt.bind
 
+let time_lock = Lwt_mutex.create ()
+
 module Event = struct
   open Util_react
 
@@ -11,27 +13,51 @@ module Event = struct
 end
 
 let get_config (state : Timedate.t) _user _body _env _state =
-  let* conf = Timedate.read_config state in
-  let js = Timedate_config.to_yojson conf in
-  Lwt.return (`Value js)
+  Lwt_mutex.with_lock time_lock (fun () ->
+      let* conf = Timedate.read_config state in
+      let* () =
+        Logs_lwt.info (fun m ->
+            m "Timedate requested config: %s" (Timedate_config.to_string conf))
+      in
+      let js = Timedate_config.to_yojson conf in
+      Lwt.return (`Value js))
 
 let get_timezones (state : Timedate.t) _user _body _env _state =
-  let js = Util_json.List.to_yojson (fun s -> `String s) state.timezones in
-  Lwt.return (`Value js)
+  Lwt_mutex.with_lock time_lock (fun () ->
+      let js = Util_json.List.to_yojson (fun s -> `String s) state.timezones in
+      let* () = Logs_lwt.info (fun m -> m "Timedate requested timezones") in
+      Lwt.return (`Value js))
 
 let set_timezone (state : Timedate.t) zone _user _body _env _state =
-  if List.exists (fun z -> z = zone) state.timezones then
-    let* () = state.time_config#set_timezone zone in
-    let* () = Timedate.push_update state in
-    Lwt.return `Unit
-  else Lwt.return (`Error ("zone " ^ zone ^ " is unknown"))
+  Lwt_mutex.with_lock time_lock (fun () ->
+      if List.exists (fun z -> z = zone) state.timezones then
+        let* () = state.time_config#set_timezone zone in
+        let* () =
+          Logs_lwt.info (fun m ->
+              m "Timedate requested timezone update: %s" zone)
+        in
+        let* () = Timedate.push_update state in
+        Lwt.return `Unit
+      else Lwt.return (`Error ("zone " ^ zone ^ " is unknown")))
 
 let set_ntp (state : Timedate.t) flag _user _body _env _state =
-  let* () = state.time_config#set_ntp flag in
-  let* () = Timedate.push_update state in
-  Lwt.return `Unit
+  Lwt_mutex.with_lock time_lock (fun () ->
+      let* () = state.time_config#set_ntp flag in
+      let* () =
+        Logs_lwt.info (fun m ->
+            m "Timedate requested ntp state update: %b" flag)
+      in
+      let* () = Timedate.push_update state in
+      Lwt.return `Unit)
 
 let set_time (state : Timedate.t) time _user _body _env _state =
-  let* () = state.time_config#set_time time in
-  let* () = Timedate.push_update state in
-  Lwt.return `Unit
+  Lwt_mutex.with_lock time_lock (fun () ->
+      let* () = state.time_config#set_time time in
+      let* () =
+        Logs_lwt.info (fun m ->
+            let tz_offset_s = Ptime_clock.current_tz_offset_s () in
+            m "Timedate requested time update: %s"
+              (Time.to_human_string ?tz_offset_s time))
+      in
+      let* () = Timedate.push_update state in
+      Lwt.return `Unit)
--- pc_control/types/timedate_config.ml
+++ pc_control/types/timedate_config.ml
@@ -6,3 +6,5 @@ type t = {
   ntp_ip : Netlib.Ipaddr.V4.t option;
 }
 [@@deriving yojson, eq]
+
+let to_string t = t |> to_yojson |> Yojson.Safe.pretty_to_string
